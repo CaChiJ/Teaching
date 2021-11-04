fe482cef2c04e7162f0250896258c8dad1c70736
CaChiJ <chj7239@gmail.com>
2021-11-05 01:19:12 +0900

    BOJ10869 comment

--- Python/Tutorial/사칙연산 - boj/BOJ10869.py
+++ Python/Tutorial/사칙연산 - boj/BOJ10869.py
@@ -1,5 +1,5 @@
-splited = input().split()
-A = int(splited[0])
+splited = input().split()   # slice string
+A = int(splited[0])     # access through index and cast string value to int
 B = int(splited[1])
 # "A, B = map(int, input().split())" work same
 
