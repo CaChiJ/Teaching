8a7c950c45532a9ecb1789e27a317fe91d116b9e
CaChiJ <chj7239@gmail.com>
2021-11-04 22:42:45 +0900

    Python Tutorial(입출력, 사칙연산) 강의 교안

---
+++ Python/Tutorial/사칙연산 - boj/BOJ10869.py
@@ -0,0 +1,10 @@
+splited = input().split()
+A = int(splited[0])
+B = int(splited[1])
+# "A, B = map(int, input().split())" work same
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
